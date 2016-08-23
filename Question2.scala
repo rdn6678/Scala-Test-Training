5863a71652a18b1956c4b8952002ba241cb9d49d
rdn6678 <renuka.dalal@crest.in>
2016-08-23 13:13:21 +0530

    Chnages done.. Printing the list

--- Question2.scala
+++ Question2.scala
@@ -1,26 +1,40 @@
+import scala.collection.mutable.ListBuffer
+
 /**
   * Created by renuka on 23/8/16.
   */
 class Question2 {
 
-  def fibo : Int = {
+  //val fiblist:List[Int] = {
 
-    var n = 0
-    var m = 1
+    def fibo: Unit = {
 
-    var i = 0
-    var sum = 0
+      var first = 0
+      var second = 1
+      val fiblist = new ListBuffer[Int]
+      var i = 0
+      var sum = 0
 
-    while(i<4000000)
-    {
-      sum = n + m
-      n = m
-      m = sum
-      i = sum
+
+      while (i < 4000000) {
+        sum = first + second
+
+        fiblist += sum
+
+        first = second
+        second = sum
+        i = sum
+
+      }
+      println(fiblist)
+
+      val sum1 = fiblist.filter(x => (x % 2  == 0)).sum
+
+      println("The sum of the even valued terms is:==" + sum1)
 
     }
-    sum
-  }
+  //}
+
 }
 
 
@@ -28,7 +42,7 @@ object Question2 extends App {
 
   val q2 = new Question2
 
-  println("The sum of the fibo series is : " + q2.fibo)
+  q2.fibo
 
 
 
