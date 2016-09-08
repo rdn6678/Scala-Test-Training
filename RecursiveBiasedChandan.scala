571d3bb96080667ab69cc3d009c1b680df30b450
rdn6678 <renuka.dalal@crest.in>
2016-09-08 10:44:19 +0530

    Completely Immutable

--- RecursiveBiasedChandan.scala
+++ RecursiveBiasedChandan.scala
@@ -1,28 +1,26 @@
-import scala.collection.mutable.{ArrayBuffer, ListBuffer}
-
 /**
   * Created by renuka on 7/9/16.
   */
 object RecursiveBiasedChandan extends App {
-  val x = new ListBuffer[Int]()
+  var x = List[Int]()
 
-  def rbc(list:List[Int]):ListBuffer[Int] = {
+  def rbc(list:List[Int]):List[Int] = {
 
 
     if (list.length > 1) {
       if (list.tail.head != 0) {
-        x += list.head
+        x ::= list.head
         //println(x)
         rbc(list.tail)
       }
       else {
-        x += 0
+        x ::= 0
         rbc(list.tail)
 
       }
     }
     else {
-      x += list.last
+      x ::= list.last
       x
     }
 
