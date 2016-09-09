d06ced3456d04788bee55035b9015da0a8793858
rdn6678 <renuka.dalal@crest.in>
2016-09-09 11:12:23 +0530

    Updated with Recurssion and Fold method

--- Pro4.scala
+++ Pro4.scala
@@ -1,20 +1,22 @@
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable.MutableList
 
 /**
   * Created by renuka on 1/9/16.
   */
 object Pro4 extends App {
 
-  def length(list: List[Int]):Int = {
-    var count = 1
-    var i = 0
-    while(i<list.length-1){
-      count+=1
-      i=i+1
+  def length(list: List[Int],c:Int):Int = {
+    if (list!=Nil){
+      length(list.tail,c+1)
     }
-    count
+    else
+    {
+      c
+    }
+  }
 
+  //Alternate method
+  def lengthCount(list:List[Int]):Int = {
+    list.foldLeft(0)((count,_)=>count+1)
   }
 
 
@@ -27,6 +29,7 @@ object Pro4 extends App {
 
   }*/
 
-  val l = List(3,4,5,2,7,9,1,4,6,8,9,9,5,34,3,2)
-  println(length(l))
+  val l = List(3,4,5,2,7,9,1,4,6,8,9,9,5,34,3,2,8,8,9)
+  println(length(l,0))
+  println(lengthCount(l))
 }
